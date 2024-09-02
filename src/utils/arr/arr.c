d13969e0baf665ba71b8310201b7a03a0b2dca25
hollanjs <jshollandsworth@gmail.com>
2024-09-02 16:53:38 -0400

    updated arr.h

--- src/utils/arr/arr.c
+++ src/utils/arr/arr.c
@@ -3,10 +3,7 @@
 #include <string.h>
 #include <stdio.h>
 
-
-void remove_index(int index, int *arr, int size);
-void remove_random_index(int *arr, int size);
-void randomize_iarr(int *arr, int size);
+#include "arr.h"
 
 
 void remove_index(int index, int *arr, int size) {
